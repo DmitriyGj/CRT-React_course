dc73d717bbd6e79d024d3f8b24e94ef42dff193e
DmitriyGj <loger865@yandex.ru>
2021-12-27 00:34:52 +0500

    Home work Home work func and class component

--- jsx_home_work/src/App.js
+++ jsx_home_work/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import pug from './pug.jpg'
+import snake from './snake.jpg'
 import CardClass  from './components/Card_class/Card_class.jsx'
 import CardFunc  from './components/Card_func/Card_func';
 
@@ -11,7 +12,7 @@ function App() {
       <CardClass name='Pretty Pug' 
       imgSrc={pug} 
       info = 'This is quite pretty pug. I really like pugs, u know'></CardClass>
-      <CardFunc name ='Ugly snake' />
+      <CardFunc name ='Ugly snake'  info = 'This is ugly snake. I really hate them'  imgSrc = {snake}/>
     </div>
   );
 }
